0f787fbf3287023c566b78581589b1b8b193534a
loispacqueteau <loispacqueteau@gmail.com>
2023-09-21 12:18:21 +0200

    Update one

---
+++ TP3Analyse/src/test/java/IUT-Blagnac/AppTest.java
@@ -0,0 +1,43 @@
+package IUT-Blagnac;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+        
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testAppAvecParametres()
+    {
+        assertEquals("Bonjour Monde!", App.hello("Bonjour Monde"));
+    }
+    public void testAppSansParametres()
+    {
+        assertEquals("Hello World!", App.hello());
+    }
+}
